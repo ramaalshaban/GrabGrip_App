01205ce0e99108326e61eeababe3d32a573256fb
Nebras <MuhammadNebras.Taleb@ogr.iu.edu.tr>
2021-09-07 16:17:23 +0300

    update: Change the app name on Android and IOS

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:grab_grip/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(GrabGripApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
